67a33ca4476a8c275c4cd275ced4ba10feb7c516
jakub.woszczyna <woszczyna.jakub@gmail.com>
2019-03-30 00:02:20 +0100

    basic list of users

--- event-support-app/src/App.js
+++ event-support-app/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import HomeNavbar from './components/navbar';
 import LoginPage from './components/login_page';
 import RegisterPage from './components/register_page';
+import MembersList from './components/members_list';
 import {BrowserRouter as Router,Route, Switch } from 'react-router-dom';
 
 class App extends Component {
@@ -14,6 +15,7 @@ class App extends Component {
        <Switch>
          <Route path="/login" component={LoginPage}/>
          <Route path="/register" component={RegisterPage}/>
+         <Route path="/members" component={MembersList}/>
        </Switch>
        </Router>
       </div>
---
+++ event-support-app/src/components/members_list/index.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Container, Row, Col, ListGroupItem} from 'react-bootstrap';
+import _ from 'lodash';
+
+export default class MembersList extends React.Component{
+
+  constructor()
+  {
+      super();
+      this.state = {
+          members: []
+      };
+  }
+
+  componentDidMount() {
+    this.getAllMembers();
+}
+
+getAllMembers() {
+ this.setState({
+   members: ["użytkownik1","użytkownik2","użytkownik3","użytkownik4","użytkownik5"]
+ })
+}
+
+renderItem(member, index) {
+  return (
+          <Row key={index}>
+              <Col></Col>
+              <Col style={{padding: "0.7%"}}>
+                  <ListGroupItem >
+                      {member}
+                      </ListGroupItem>
+              </Col>
+              <Col></Col>
+          </Row>
+  )
+}
+
+
+  render() {
+    const members = _.map(this.state.members, (member, k) => {
+        return this.renderItem(member, k);
+    });
+    return (
+      <div>
+    <h1>Lista użytkowników:</h1>
+    <Container style={{width: "60%"}}>
+     {members}
+    </Container>
+    </div>
+    );
+}
+}
