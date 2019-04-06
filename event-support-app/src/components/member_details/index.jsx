8ab44ba079b1d2267b438eeaa7d68c738330dd82
jakub.woszczyna <woszczyna.jakub@gmail.com>
2019-04-06 19:32:48 +0200

    changed viewing and changing member id after clicking

--- event-support-app/src/components/member_details/index.jsx
+++ event-support-app/src/components/member_details/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container, Row, Col, ListGroupItem} from 'react-bootstrap';
 import _ from 'lodash';
 import './style.css'
+import MembersList from '../members_list/index';
 
 export default class MemberDetails extends React.Component{
 
@@ -9,38 +10,31 @@ export default class MemberDetails extends React.Component{
   {
       super();
       this.state = {
-          member: {id: 0, name: "Adam", surname: "Kowalski", email: "sample@mail.com", address: "Wrocław, Plac Grunwaldzki"},
-          members: []
+          member: {id: 5, name: "Adam", surname: "Kowalski", email: "sample@mail.com", address: "Wrocław, Plac Grunwaldzki"},
+          member2: {id: 1, name: "Krzys", surname: "Nowak", email: "sample2@mail.com", address: "Wrocław, Plac Grunwaldzki"}
       };
   }
 
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.match.params.id != this.state.member.id) {
+      this.getMemberDetails(nextProps.match.params.id);
+    }
+  }
+
   componentDidMount() {
+    console.log("jest")
     this.getMemberDetails();
 }
 
-getMemberDetails() {
+getMemberDetails(nextProps = this.props.match.params.id ) {
  this.setState(prevState => ({
   member: {
-    id: this.props.match.params.id,
-    ...prevState.member
-  },
-   members: ["użytkownik1","użytkownik2","użytkownik3","użytkownik4","użytkownik5"]
+    ...prevState.member,
+    id: nextProps
+  }
  }));
 }
 
-renderMemberInList(member, index) {
-  return (
-          <Row key={index}>
-              <Col></Col>
-              <Col className ="memberList">
-                  <ListGroupItem>
-                      {member}
-                      </ListGroupItem>
-              </Col>
-              <Col md="2"></Col>
-          </Row>
-  )
-}
 
 // renderMemberDetails(){
 //   console.log("DUPADUPA");
@@ -50,27 +44,23 @@ renderMemberInList(member, index) {
 // }
 
   render() {
-    const members = _.map(this.state.members, (member, k) => {
-      return this.renderMemberInList(member, k);
-  });
 
   return (
     <div id="Page">
   
   <div id="Members">
-  <h1>Lista użytkowników:</h1>
-   {members}
+  <MembersList/>
   </div>
   <div id="MemberDetails">
 <h2>Szczegóły o użytkowniku:</h2>
   <Row>
               <Col>
               <ListGroupItem>
-                <b classname="detailsHeader">ID: </b>
+                <b className="detailsHeader">ID: </b>
                       {this.state.member.id}
                       </ListGroupItem>
                       <ListGroupItem>
-                      <b classname="detailsHeader">Imię: </b>
+                      <b className="detailsHeader">Imię: </b>
                       {this.state.member.name}
                       </ListGroupItem>
               </Col>
