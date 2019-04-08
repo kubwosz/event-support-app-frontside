92ab27837fa47c9886cfeb19f8b73de4f4c2f309
jakub.woszczyna <woszczyna.jakub@gmail.com>
2019-04-08 12:15:58 +0200

    added references and routing to the events list

--- event-support-app/src/App.js
+++ event-support-app/src/App.js
@@ -4,6 +4,7 @@ import HomeNavbar from './components/navbar';
 import LoginPage from './components/login_page';
 import RegisterPage from './components/register_page';
 import MembersList from './components/members_list';
+import EventsList from './components/events_list';
 import MemberDetails from './components/member_details';
 import {BrowserRouter as Router,Route, Switch } from 'react-router-dom';
 
@@ -17,6 +18,7 @@ class App extends Component {
          <Route exact path="/login" component={LoginPage}/>
          <Route exact path="/register" component={RegisterPage}/>
          <Route exact path="/members" component={MembersList}/>
+         <Route exact path="/events" component={EventsList}/>
          <Route exact path="/memberDetails/:id" component={MemberDetails}/>
          <Route path="/" component={RegisterPage}/>
        </Switch>
