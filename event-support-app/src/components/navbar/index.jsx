5b913c48cbd2d8d6ebcf7b48361b30677ed8a801
jakub.woszczyna <woszczyna.jakub@gmail.com>
2019-04-08 12:22:42 +0200

    changed navbar routing

--- event-support-app/src/components/navbar/index.jsx
+++ event-support-app/src/components/navbar/index.jsx
@@ -6,15 +6,14 @@ export default class HomeNavbar extends React.Component{
     render(){
         return(
             <Navbar bg="dark" variant="dark">
-    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Navbar.Brand href="/">Home</Navbar.Brand>
     <Nav className="mr-auto">
-      <Nav.Link href="/home">Home</Nav.Link>
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="/members">Show members</Nav.Link>
+      <Nav.Link href="/events">Wydarzenia</Nav.Link>
+      <Nav.Link href="/members">Lista użytkowników</Nav.Link>
     </Nav>
     <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-      <Button variant="outline-info">Search</Button>
+      <FormControl type="text" placeholder="Szukaj" className="mr-sm-2" />
+      <Button variant="outline-info">Szukaj</Button>
     </Form>
   </Navbar>
         )
