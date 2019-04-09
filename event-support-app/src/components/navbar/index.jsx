200c09a97dbae2aaf85121bd39627d536c80e483
jakub.woszczyna <woszczyna.jakub@gmail.com>
2019-04-09 14:49:42 +0200

    added to navbar login and sign in icons

--- event-support-app/src/components/navbar/index.jsx
+++ event-support-app/src/components/navbar/index.jsx
@@ -1,21 +1,33 @@
-import React from 'react';
-import { Navbar, Nav, NavItem, Form, FormControl,Button } from 'react-bootstrap';
+import React from "react";
+import { Navbar, Nav, NavItem } from "react-bootstrap";
+import { FormControl, Button, Form } from "react-bootstrap";
 
-
-export default class HomeNavbar extends React.Component{
-    render(){
-        return(
-            <Navbar bg="dark" variant="dark">
-    <Navbar.Brand href="/">Home</Navbar.Brand>
-    <Nav className="mr-auto">
-      <Nav.Link href="/events">Wydarzenia</Nav.Link>
-      <Nav.Link href="/members">Lista użytkowników</Nav.Link>
-    </Nav>
-    <Form inline>
-      <FormControl type="text" placeholder="Szukaj" className="mr-sm-2" />
-      <Button variant="outline-info">Szukaj</Button>
-    </Form>
-  </Navbar>
-        )
-    }
+export default class HomeNavbar extends React.Component {
+  render() {
+    return (
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="/">Home</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="/events">Wydarzenia</Nav.Link>
+          <Nav.Link href="/members">Lista użytkowników</Nav.Link>
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="Szukaj" className="mr-sm-2" />
+          <Button variant="outline-info">Szukaj</Button>
+        </Form>
+        <ul class="nav navbar-nav navbar-right">
+          <li>
+            <a href="#">
+              <span class="glyphicon glyphicon-user" /> Zarejestruj się
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <span class="glyphicon glyphicon-log-in" /> Zaloguj się
+            </a>
+          </li>
+        </ul>
+      </Navbar>
+    );
+  }
 }
